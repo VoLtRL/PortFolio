3d5de6fb6707f2d9059a9117fbc29c0e91e87726
VoLtRL <raphael.lecaille@gmail.com>
2025-07-31 03:25:54 +0200

    added js file and updated css

---
+++ portfolio/templates/script.js
@@ -0,0 +1,12 @@
+const body = document.body;
+
+const init = function () {
+  loadInterface();
+};
+
+const loadInterface = function () {
+  const load_div = document.createElement("div");
+  load_div.id = "loading";
+};
+
+init();

18ed2aebc2e8c76386fe75bb5eb1d7ee4120a4db
VoLtRL <raphael.lecaille@gmail.com>
2025-08-01 00:26:46 +0200

    added the loading animation <3

--- portfolio/templates/script.js
+++ portfolio/templates/script.js
@@ -1,12 +1,25 @@
 const body = document.body;
+const username = "Cliff";
+const password = "************";
+const speed = 100;
 
-const init = function () {
-  loadInterface();
-};
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-const loadInterface = function () {
-  const load_div = document.createElement("div");
-  load_div.id = "loading";
-};
+async function typing(text, selection) {
+  const element = document.getElementById(selection);
+  for (let i = 0; i < text.length; i++) {
+    element.innerHTML += text.charAt(i);
+    await sleep(speed);
+  }
+}
+
+async function init() {
+  await typing(username, "username");
+  await typing(password, "password");
+  body.getElementsByClassName("loader")[0].style.animation =
+    "hideLoader 1s ease-out forwards";
+}
 
 init();
